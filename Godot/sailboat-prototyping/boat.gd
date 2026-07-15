93e1589f759a46650a8079d7b11f216e7ed633dc
gabrielplatte <75038637+gabrielplatte@users.noreply.github.com>
2026-07-15 23:31:31 +1000

    More Godot prototyping
    YALL i think i'm getting somewhere with the boat physics. good balance of real feeling and stable. we shall seeee

--- Godot/sailboat-prototyping/boat.gd
+++ Godot/sailboat-prototyping/boat.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
 @onready var floaters: Array[RayCast3D] = [%floater0, %floater1]
+@onready var force_vector: Vector2
+@onready var force_vector_3d
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +11,15 @@ func _ready():
 func _physics_process(delta):
 	for floater in floaters:
 		floater.global_rotation = Vector3.ZERO
-		do_buoyancy(floater)
+		#do_buoyancy(floater)
 		#print(floater.global_rotation)
+
+		force_vector = Input.get_vector("left", "right", "forward", "back")
+	force_vector_3d = to_local(Vector3(force_vector.x, 0, force_vector.y))
+	apply_force(force_vector_3d*0.2, (to_local(Vector3(0, 2, -2))))
+	#print(force_vector)
+
+	do_righting_forces()
 
 func do_buoyancy(floater: RayCast3D) -> void:
 	if floater.is_colliding():
@@ -21,3 +30,5 @@ func do_buoyancy(floater: RayCast3D) -> void:
 		var buoyancy = Vector3.UP * (floater_depth - floater_length)
 		apply_force(buoyancy * 1, floater.position)
 		print(buoyancy)
+func do_righting_forces():
+	print(global_transform.basis.x)
