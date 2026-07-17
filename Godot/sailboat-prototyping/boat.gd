8227a31ba9385a6c049e4b11860df266be029239
gabrielplatte <75038637+gabrielplatte@users.noreply.github.com>
2026-07-17 18:02:59 +1000

    Added the DebugDraw addon to the Godot project

--- Godot/sailboat-prototyping/boat.gd
+++ Godot/sailboat-prototyping/boat.gd
@@ -1,8 +1,11 @@
 extends RigidBody3D
 
-@onready var floaters: Array[RayCast3D] = [%floater0, %floater1]
-@onready var force_vector: Vector2
-@onready var force_vector_3d
+@onready var floaters: Array[RayCast3D] = [%floater0, %floater1, %floater2]
+@onready var input_vector: Vector2
+@onready var input_vector_to_3d: Vector3
+@onready var force_vector = to_local(Vector3())
+@export var wind_scale: float
+@export var wind_position = Vector3()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,12 +14,15 @@ func _ready():
 func _physics_process(delta):
 	for floater in floaters:
 		floater.global_rotation = Vector3.ZERO
-		#do_buoyancy(floater)
-		#print(floater.global_rotation)
+		do_buoyancy(floater)
+		print(floater.global_rotation)
 
-		force_vector = Input.get_vector("left", "right", "forward", "back")
-	force_vector_3d = to_local(Vector3(force_vector.x, 0, force_vector.y))
-	apply_force(force_vector_3d*0.2, (to_local(Vector3(0, 2, -2))))
+	input_vector = Input.get_vector("left", "right", "forward", "back")
+	input_vector_to_3d = Vector3(input_vector.x, 0, input_vector.y)
+	force_vector = input_vector_to_3d * wind_scale
+
+	apply_force(force_vector, wind_position)
+	DebugDraw3D.draw_arrow_ray(wind_position, force_vector, force_vector.length())
 	#print(force_vector)
 
 	do_righting_forces()
@@ -25,10 +31,10 @@ func do_buoyancy(floater: RayCast3D) -> void:
 	if floater.is_colliding():
 		var collision_point = floater.get_collision_point()
 		var floater_length = - floater.target_position.length()
-		var floater_depth = floater.global_position.y - collision_point.y
+		var floater_depth = collision_point.y - floater.global_position.y
 		#print(floater_depth)
-		var buoyancy = Vector3.UP * (floater_depth - floater_length)
-		apply_force(buoyancy * 1, floater.position)
+		var buoyancy = Vector3.UP * floater_depth
+		apply_force(buoyancy, floater.position + to_local(Vector3(0, 0.6, 0)))
 		print(buoyancy)
 func do_righting_forces():
 	print(global_transform.basis.x)
