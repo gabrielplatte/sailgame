2a74f7d72e6c2d576b86d564faeba0f8145f4c98
gabrielplatte <75038637+gabrielplatte@users.noreply.github.com>
2026-08-15 21:18:39 +1000

    More work on water

---
+++ Godot/water-shader-testing/Scenes/log.gd
@@ -0,0 +1,32 @@
+extends RigidBody3D
+
+@export var float_force := 1.0
+@export var water_drag := 0.05
+@export var water_angular_drag := 0.05
+
+@onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+const water_height := 0.0
+
+var submerged := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta):
+	submerged = false
+	var depth = water_height - global_position.y
+	if depth > 0:
+		submerged = true
+		apply_central_force(Vector3.UP * float_force * gravity * depth)
+
+func _integrate_forces(state: PhysicsDirectBodyState3D):
+	if submerged:
+		state.linear_velocity *= 1 - water_drag
+		state.angular_velocity *= 1 - water_angular_drag
--- Godot/water-shader-testing/Scenes/water_plane.gd
+++ Godot/water-shader-testing/Scenes/water_plane.gd
@@ -13,6 +13,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_up"):
 		
 		height_scalar -= Input.get_axis("ui_up", "ui_down") / 500
-		height_scalar = clamp(height_scalar, 0, 0.5)
+		height_scalar = clamp(height_scalar, 0, 0.15)
 		
 		get_active_material(0).set_shader_parameter("height_scale", height_scalar)
